412b703fc4ecc062ce3d227f732139cd41c8cd73
palomapaine1 <palomapaine4@gmail.com>
2024-11-27 19:59:22 -0300

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -20,8 +20,8 @@ def obtener_datos_api(api_url):
     else:
         st.error(f"Error al obtener los datos de la API: {response.status_code}")
         return None
-        # Página de interacción con los datos
-def pagina_interaccion():
+# Página de interacción con los datos
+def pagina_interaccion()
     api_url = "https://restcountries.com/v3.1/all"
     df = obtener_datos_api(api_url)
     
