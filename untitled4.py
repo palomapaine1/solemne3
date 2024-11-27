9b7e49b005d01f130f98fec7fa39dadf793baaa4
palomapaine1 <palomapaine4@gmail.com>
2024-11-27 19:02:47 -0300

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -23,6 +23,13 @@ def obtener_datos_api(api_url):
     else:
         st.error(f"Error al obtener los datos de la API: {response.status_code}")
         return None
+# Llamar la función para obtener los datos
+api_url = "https://restcountries.com/v3.1/all"
+df = obtener_datos_api(api_url)
+# Si hay datos, mostrar el DataFrame, mostrar dataframe con las columna seleccionadas, permitir filtrado y mostrar gráficos.
+
+if df is not None:
+    st.write(df.head())
 
 # Título de la aplicación
 st.title('Aplicación Web: Datos desde una API REST')
